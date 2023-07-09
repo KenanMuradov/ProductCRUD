001a5c70adeaf12736dd9316e988ced535b9db70
Kenan Muradov <maljerov.1999@gmail.com>
2023-07-09 18:29:50 +0300

    Beta 2.0

--- ProductCRUD/Controllers/ProductController.cs
+++ ProductCRUD/Controllers/ProductController.cs
@@ -5,14 +5,7 @@ namespace ProductCRUD.Controllers
 {
     public class ProductController : Controller
     {
-        private static List<Product> _products = new() {
-            new() { Id = 1,Name="Cola",Category="Drinks", Price=2.99,Count=99,Description="Somesadfadsfadsfadsfas Cola"},
-            new() { Id = 1,Name="Cola",Category="Drinks", Price=2.99,Count=99,Description="Someasdfdsa Cola"},
-            new() { Id = 1,Name="Cola",Category="Drinks", Price=2.99,Count=99,Description="Sgdfg"},
-            new() { Id = 1,Name="Cola",Category="Drinks", Price=2.99,Count=99,Description="Some Csdafasdfasdola"},
-            new() { Id = 1,Name="Cola",Category="Drinks", Price=2.99,Count=99,Description="Some Cola"},
-            new() { Id = 1,Name="Cola",Category="Drinks", Price=2.99,Count=99,Description="Somasdfasdfadsfsae Cola"}
-        };
+        private static List<Product> _products = new();
 
         public IActionResult Index()
         {
--- ProductCRUD/Models/Product.cs
+++ ProductCRUD/Models/Product.cs
@@ -8,6 +8,6 @@
         public string? Category { get; set; }
         public double Price { get; set; }
         public int Count { get; set; }
-        public string? ImageLink { get; set; }
+        public string? ImageLink { get; set; } = null;
     }
 }
